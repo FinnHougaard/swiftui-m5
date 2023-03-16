e9f18180295831b4d9157972f72af8c8ca297d5a
Finn Hougaard <finnh@email.dk>
2023-03-16 16:38:04 +0100

    Initial Commit

---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by Finn Hougaard on 16/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
